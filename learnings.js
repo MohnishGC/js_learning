86ce1c0264202a7283eaf4fdfda2e3d5aa3c3ba9
Mohnish Budhabaware <victor27shelley@gmail.com>
2023-12-12 21:49:22 +0530

    added react

--- learnings.js
+++ learnings.js
@@ -1,4 +1,21 @@
-console.log("HI Yash");
-let name01 = "Mohnish";
-console.log(name01);
-alert(name01);
+// console.log("HI Yash");
+// let name01 = "Mohnish";
+// console.log(name01);
+// alert(name01);
+
+function MyButton() {
+    return (
+        <button>
+            I'm a button
+        </button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
